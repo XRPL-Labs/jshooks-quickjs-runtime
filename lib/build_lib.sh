be35a0e4195b1309c0da8b0ebbfd9777cb68d902
csh <458761603@qq.com>
2021-08-26 01:18:18 +0800

    fix for bindgen.rs

--- lib/build_lib.sh
+++ lib/build_lib.sh
@@ -3,8 +3,8 @@
 rm *.o
 wasicc -c ../libbf.c ../cutils.c ../libunicode.c wapper.c ../libregexp.c ../quickjs-libc.c -DCONFIG_BIGNUM=1 -DCONFIG_VERSION='"wasi"' -D_WASI_EMULATED_SIGNAL -lwasi-emulated-signal -O3
 wasiar -r libquickjs.a *.o
-bindgen wapper.h -o binding.rs
+bindgen wapper.h -o binding.rs -- -D__wasi__
 # cp static-lib and binding.rs to quickjs-rs-wasi
 # change cp path
-cp libquickjs.a ../../quickjs-rs-wasi/lib/
-cp binding.rs ../../quickjs-rs-wasi/lib/
+# cp libquickjs.a ../../quickjs-rs-wasi/lib/
+# cp binding.rs ../../quickjs-rs-wasi/lib/
