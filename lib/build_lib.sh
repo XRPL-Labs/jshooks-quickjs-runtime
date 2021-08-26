468d2d79622c183e14bfba55b2a2e5c5182732d9
csh <458761603@qq.com>
2021-08-26 20:49:38 +0800

    pref: bindgen with --sizie_t-is-usize

--- lib/build_lib.sh
+++ lib/build_lib.sh
@@ -3,7 +3,7 @@
 rm *.o
 wasicc -c ../libbf.c ../cutils.c ../libunicode.c wapper.c ../libregexp.c ../quickjs-libc.c -DCONFIG_BIGNUM=1 -DCONFIG_VERSION='"wasi"' -D_WASI_EMULATED_SIGNAL -lwasi-emulated-signal -O3
 wasiar -r libquickjs.a *.o
-bindgen wapper.h -o binding.rs -- -D__wasi__
+bindgen wapper.h --size_t-is-usize -o binding.rs -- -D__wasi__
 # cp static-lib and binding.rs to quickjs-rs-wasi
 # change cp path
 # cp libquickjs.a ../../quickjs-rs-wasi/lib/
